bcfccab2cafa0163c8305286d83eb1a44bc5ba82
Marty <mataholl@gmail.com>
2023-05-29 12:50:48 +0200

    HOTOVO

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Subscriber = require('../models/subscriber')
+const subscriber = require('../models/subscriber')
 
 router.get('/', async (req, res) => {
       try {
@@ -12,11 +13,11 @@ router.get('/', async (req, res) => {
     })
     
 
-router.get('/:id', (req, res) => {
-      res.send(req.params.id)
+router.get('/:id',getSubscriber, (req, res) => {
+      res.send(res.subscriber)
 })
 
-router.post('/', async (req, res) => {
+router.post('/',async (req, res) => {
       const subscriber = new Subscriber({
             name: req.body.name,
             subscribedToChannel: req.body.subscribedToChannel
@@ -30,11 +31,42 @@ router.post('/', async (req, res) => {
       }
 })
 
-router.patch('/:id', (req, res) => {
-      
+router.patch('/:id', getSubscriber, async (req, res) => {
+      if (req.body.name != null) {
+        res.subscriber.name = req.body.name
+      }
+      if (req.body.subscribedToChannel != null) {
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+      }
+      try {
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
+    })
+
+router.delete('/:id', getSubscriber, async (req, res) => {
+      try{
+            await res.subscriber.deleteOne()
+            res.json({ message: 'Odběratel odstraněn '})
+      }catch (err){
+            res.status(500).json({ message: err.message })
+      }
 })
 
-router.delete('/:id', (req, res) => {
-      
-})
+async function getSubscriber(req, res, next){
+      let subscriber
+      try{
+            subscriber = await Subscriber.findById(req.params.id)
+            if(subscriber == null){
+                  return res.status(404).json({ message: 'Nelze najít odběratele ' })
+            }
+      }catch (err){
+            return res.status(500).json({ message: err.message })
+      }
+      res.subscriber = subscriber
+      next()
+}
+
 module.exports = router
